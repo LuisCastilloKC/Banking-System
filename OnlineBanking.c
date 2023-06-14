acc768ea3088c166b6300c75ccb658db7c1b2848
Luis Castillo <mr.luiscastillo@gmail.com>
2023-06-13 22:32:13 -0400

    add print message

--- OnlineBanking.c
+++ OnlineBanking.c
@@ -12,6 +12,23 @@ struct user
 
 
 int main(){
-    
+    struct user usr;
+    int opt;
+
+    printf("\n What would you you like to do?");
+    printf("\n\n1. Register an Account");
+    printf("\n2. Login to an account");
+
+    printf("\n\nEnter your choice:\t");
+    scanf("%d", &opt);
+
+    if(opt == 1){
+        system("clear");
+        printf("Enter your account number:\t");
+        scanf("%s", usr.account);
+        printf("\nEnter your phone number:\t");
+        scanf("%s", usr.phone);
+    }
+
     return 0;
 }
