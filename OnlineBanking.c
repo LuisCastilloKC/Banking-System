5ede987ff5aa1d4fc7156ef8caa6756525264f28
Luis Castillo <mr.luiscastillo@gmail.com>
2023-06-13 22:16:13 -0400

    define user

--- OnlineBanking.c
+++ OnlineBanking.c
@@ -2,6 +2,15 @@
 #include<stdlib.h>
 #include<string.h>
 
+struct user
+{
+    char phone[50];
+    char account[50];
+    char password[50];
+    float balance;
+};
+
+
 int main(){
     
     return 0;
